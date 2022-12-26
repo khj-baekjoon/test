2e6cabf4be7bc3235691689bea5b6dda1ec3c4e6
khj-baekjoon <hyukjoon1215@naver.com>
2022-12-26 21:18:05 +0900

    test

---
+++ study/main.cpp
@@ -0,0 +1,17 @@
+//깃허브 테스트
+#include <iostream>
+
+
+
+int main() {
+
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+	
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << a + b;
+
+	return 0;
+}
